c534e293d583a6959a93e52033dc8b30beda0d13
modengann <99417989+modengann@users.noreply.github.com>
2022-09-18 21:58:27 -0400

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/** Task 1:
+ * YouTube link: https://youtu.be/4Wzc3vYd8dk?list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM
+ * Goal: When you click, a Text object AND an oval will appear at the mouse location
+ */
+
+/** Task 2:
+ * YouTube link: https://youtu.be/vcO0AaL6VRY?list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM
+ * Goal: Create a Rectangle object as a global variable. Modify that Rectangle object when you use input (click, key press, etc)
+ */
+import javadraw.*;
+public class Main extends Window{
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
